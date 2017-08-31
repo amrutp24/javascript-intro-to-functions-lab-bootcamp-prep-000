7f5e4a06f7ca586c6b14248543a507c4f2af1a12
a p <amrut.pagidipally@gmail.com>
2017-08-31 07:29:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function logWhisper(string) {
   console.log('hello')
 }
 function sayHiToGrandma(string) {
-  return 'HELLO'.toLowerCase()
+  return "I can\'t hear you!".toLowerCase()
 }
 function sayHiToGrandma(string) {
   return string.toUpperCase()
