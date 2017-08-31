79c5bf467672f95bb28db980d8f9fe7a458b2aac
a p <amrut.pagidipally@gmail.com>
2017-08-31 07:26:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function logShout(string){
 function logWhisper(string) {
   console.log('hello')
 }
-function sayHiToGrandma(string) {
+function sayHiToGrandma('HELLO') {
   return 'HELLO'.toLowerCase()
 }
 function sayHiToGrandma(string) {
